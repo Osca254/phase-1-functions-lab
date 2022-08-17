a9c508225d6e834d38303bddfb4d68107e4f6f0b
moringa <oscarokumu47@gmail.com>
2022-08-17 17:23:28 +0300

    modify index.js && package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+  return Math.abs(blocks-42);
+}
+console.log(distanceFromHqInBlocks(43));
+
+function distanceFromHqInFeet(blocks){
+  return blocks+221;
+}
+console.log(distanceFromHqInFeet(43));
+function distanceTravelledInFeet(distance1,distance2){
+  let block = distance1+distance2;
+  return block+1229;
+}
+console.log(distanceTravelledInFeet(43,48));
+
+function calculatesFarePrice(start, destination){
+  
+}
