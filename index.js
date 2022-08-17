d43d6bd227002c39c1f9d338d5585411163713ff
moringa <oscarokumu47@gmail.com>
2022-08-17 19:13:14 +0300

    modify index.js && package-lock.json

--- index.js
+++ index.js
@@ -5,15 +5,34 @@ function distanceFromHqInBlocks(blocks){
 console.log(distanceFromHqInBlocks(43));
 
 function distanceFromHqInFeet(blocks){
-  return blocks+221;
+  return distanceFromHqInBlocks(blocks)*264
 }
+distanceFromHqInFeet(43);
+
 console.log(distanceFromHqInFeet(43));
-function distanceTravelledInFeet(distance1,distance2){
-  let block = distance1+distance2;
-  return block+1229;
+
+function distanceTravelledInFeet(start ,destination){
+  if(destination>start){
+    return (destination - start)*264;
+  } else{
+    return (start - destination)*264;
+  }
+  
 }
 console.log(distanceTravelledInFeet(43,48));
 
 function calculatesFarePrice(start, destination){
-  
+  let distance= distanceTravelledInFeet(start,destination);
+  if(distance<=2000){
+    return 0;
+  }else if(distance<2000){
+    return (distance -400 )*0.02;
+  }else if(distance>=2000 && distance<2500){
+    return 25;
+  }else if (distance>=2500){
+    return "cannot travel that far";
+  }
 }
+calculatesFarePrice(43,44);
+calculatesFarePrice(50,58);
+calculatesFarePrice(34,24)
